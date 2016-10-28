4e491dd10808993c1cad24fbe0fe5e644420a2d9
jagrutipatil <jagrutipatil32@gmail.com>
2016-10-28 14:38:30 -0700

    Added Street View: Hardcoded lat-long

--- SJSUMap/app/src/main/java/edu/sjsu/sjsumap/activity/DetailInfoActivity.java
+++ SJSUMap/app/src/main/java/edu/sjsu/sjsumap/activity/DetailInfoActivity.java
@@ -1,7 +1,12 @@
 package edu.sjsu.sjsumap.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.SyncStateContract;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -9,6 +14,7 @@ import android.widget.TextView;
 import edu.sjsu.sjsumap.R;
 import edu.sjsu.sjsumap.model.BuildingInfo;
 import edu.sjsu.sjsumap.model.LocationService;
+import edu.sjsu.sjsumap.tasks.StreetViewTask;
 
 //TODO backpress
 
@@ -38,7 +44,15 @@ public class DetailInfoActivity extends AppCompatActivity {
         time.setText(buildingInfo.getTime());
 
         locImage.setImageResource(buildingInfo.getImageId());
+
+        //TODO harcoded lat, long
+        streetView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent streetView = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse("google.streetview:cbll="+-33.87365+","+151.20689+"&cbp=1,99.56,,1,-5.27&mz=21"));
+                streetView.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(streetView);
+            }
+        });
     }
-
-
 }
